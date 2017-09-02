0b70476444cab59fcc7d339b071c66838f53d595
Serhii <Soboliev>
2017-09-03 02:05:06 +0300

    Phase

--- fft/view/sbkfft1dview.m
+++ fft/view/sbkfft1dview.m
@@ -32,10 +32,10 @@ function inputFunction_Callback(hObject, eventdata, handles)
 
 function drawSpatial(hObject, handles)   
     funcType = get(handles.inputFunction, 'value');
-    freq = str2num(get(handles.textFrequency, 'String'));
-    sampFreq = str2num(get(handles.textSamplingFrequency, 'String'));  
+    freq = round(str2double(get(handles.textFrequency, 'String')));
+    sampFreq = 32*freq;  
     Ts = 1/sampFreq;  
-    x = 1:Ts:3;
+    x = -2:Ts:2;
     switch funcType
         case 1            
              y = sin(2*pi*freq*x);
@@ -48,58 +48,40 @@ function drawSpatial(hObject, handles)
     set(handles.inputFunction,'UserData', struct('x',x,'y',y)); 
     guidata(hObject, handles);
     
-function drawFrequency(~, handles)
-    f = str2num(get(handles.textFrequency, 'String'));
-    fs = str2num(get(handles.textSamplingFrequency, 'String'));
-    data = get(handles.inputFunction, 'UserData');
-    x = data.x;
+function drawFrequency(~, handles)   
+    fs = 32*str2double(get(handles.textFrequency, 'String'));
+    data = get(handles.inputFunction, 'UserData'); 
     y = data.y;
     samplesCount = length(y);
-    nfft2 = 2^nextpow2(samplesCount);
+    nfft2 = 2^(nextpow2(samplesCount)-1);
     ff = fft(y, nfft2);
     fff = ff(1:nfft2/2);
     xfft = fs * (0 : nfft2/2 - 1) / nfft2;
     showOnAxis(handles.frequencyMagnitudeAxis, xfft, abs(fff));
+    axes(handles.frequencyMagnitudeAxis);
+    plot(xfft,abs(fff));  
+    axis([0,530,min(abs(fff)),1.1*max(abs(fff))]);
+    
+    axes(handles.frequencyPhaseAxis);
+    threshold = max(abs(fff))/10000;
+    y2 = y;
+    y2(abs(y)<threshold) = 0;
+    phase = angle(y2);
+    plot(xfft,phase(1:nfft2/2));
 
 function inputFunction_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to inputFunction (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
 
 function doFourierTransform_Callback(hObject, eventdata, handles)
-    f = str2num(get(handles.textFrequency, 'String'));
-    fs = str2num(get(handles.textSamplingFrequency, 'String'));
-    data = get(handles.inputFunction, 'UserData');
-    x = data.x;
-    y = data.y;
-%     nfft2 = 2^nextpow2(length(x));
-%     fft_y = fft(y, nfft2);
-%     fff = fft_y(1:nfft2/2);
-%     xfft = f * (0 : nfft2/2 - 1) / nfft2;
-% 
-%     %SbkFFT = Sbk1dFFT(y);
-%     %fft_y = doFFT(SbkFFT);
-%     abs_fft_y = abs(fft_y);
-%     % phase_fft_y = 
-%    showOnAxis(handles.frequencyMagnitudeAxis, x, fft(y));
-    
-    samplesCount = length(y);
-    nfft2 = 2^nextpow2(samplesCount);
-    ff = fft(y, nfft2);
-    fff = ff(1:nfft2/2);
-    xfft = fs * (0 : nfft2/2 - 1) / nfft2;
-    showOnAxis(handles.frequencyMagnitudeAxis, xfft, abs(fff));   
+    drawFrequency(hObject, handles);  
    
 function showOnAxis(axesId,x,y)
     axes(axesId);
     plot(x,y);
-   % axis(bounds);   
+   % axis(bounds);  
+   
 function textFrequency_Callback(hObject, eventdata, handles)
 
 function textFrequency_CreateFcn(hObject, eventdata, handles)
